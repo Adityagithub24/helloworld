d7741ba20571194dad99bbcc99c9093345de0bb5
Adityagithub24 <adityakpurohit.mds@gmail.com>
2022-02-03 17:54:24 +0530

    my first hellow world java from eclipse

---
+++ helloworld/src/helloworld/main/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworld.main;
+
+/**
+ * @author adity
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello welcome to eclipse");
+	}
+
+}
